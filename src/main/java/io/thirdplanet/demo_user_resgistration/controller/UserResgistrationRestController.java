8702083e9101c2bf11d1c6dda78aa96229ced7e0
popasmuerf <MichaelBrown172@Gmail.com>
2018-12-17 23:32:39 -0500

    Added error catching code for all controller methods....

--- src/main/java/io/thirdplanet/demo_user_resgistration/controller/UserResgistrationRestController.java
+++ src/main/java/io/thirdplanet/demo_user_resgistration/controller/UserResgistrationRestController.java
@@ -36,8 +36,14 @@ public class UserResgistrationRestController {
 
     @GetMapping("/")
     public ResponseEntity<List<UserDTO>> listAllUsers() {
+
         List<UserDTO> users = userJpaRepository.findAll();
-        return new ResponseEntity <List<UserDTO>>(users,HttpStatus.OK);
+
+        if(users != null && !users.isEmpty()){
+            return new ResponseEntity <List<UserDTO>>(users,HttpStatus.OK);
+        }else{
+            return new ResponseEntity <List<UserDTO>>(users,HttpStatus.NO_CONTENT);
+        }
     }
 
     @GetMapping("/{id}")
@@ -55,19 +61,31 @@ public class UserResgistrationRestController {
 
     @PostMapping(value="/",consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<UserDTO> createUser(@RequestBody final UserDTO user){
-        userJpaRepository.save(user);
-        return new ResponseEntity<UserDTO>(user,HttpStatus.CREATED);
+        Long userId = user.getId() ;
+        Optional<UserDTO> optUserDTO = userJpaRepository.findById(userId);
+        if(optUserDTO.isPresent()){
+            CustomErrorType errorUserDTO = new CustomErrorType("Cannot create this user...they might already exist...") ;
+            return new ResponseEntity<UserDTO>(errorUserDTO,HttpStatus.CONFLICT);
+        }else {
+            userJpaRepository.save(user);
+            return new ResponseEntity<UserDTO>(user, HttpStatus.CREATED);
+        }
     }
+
+
     @PutMapping(value="/{id}", consumes =  MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<UserDTO> updateUser(@PathVariable("id") final Long id,@RequestBody UserDTO user){
-        Optional<UserDTO> userDTOOpt = userJpaRepository.findById(id) ;
+        Optional<UserDTO> optUserDTO = userJpaRepository.findById(id) ;
         UserDTO userDTO  = new UserDTO();
-        if(userDTOOpt.isPresent()){
-            userDTO = userDTOOpt.get() ;
+        if(optUserDTO.isPresent()){
+            userDTO = optUserDTO.get() ;
             userDTO.setName(user.getName());
             userDTO.setAddress(user.getAddress());
             userDTO.setEmail(user.getEmail());
             userJpaRepository.saveAndFlush(userDTO);
+        }else{
+            CustomErrorType customErrorType = new CustomErrorType("unknown user....likely does not exist...");
+            ResponseEntity<UserDTO> userDTOResponseEntity = new ResponseEntity<UserDTO>(customErrorType,HttpStatus.NOT_FOUND);
         }
         return new  ResponseEntity<UserDTO>(userDTO,HttpStatus.OK);
     }
