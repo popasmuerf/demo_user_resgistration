56bdbca84f060036ebee57f41e39c2756ca63601
mikeyb <MichaelBrown172@Gmail.com>
2019-02-13 15:37:35 -0500

    ....

---
+++ src/main/java/io/thirdplanet/demo_user_resgistration/domain/MarathonParams.java
@@ -0,0 +1,15 @@
+package io.thirdplanet.demo_user_resgistration.domain;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by mikeyb on 2/13/19.
+ */
+@Entity
+public class MarathonParams {
+    private String COMPANY_NAME ;
+    private String OBSERVED_SYSTEM_NAME;
+    private String OBSERVED_SYSTEM_API ;
+    private String OBSERVED_SYSTEM_SECRET ;
+
+}
