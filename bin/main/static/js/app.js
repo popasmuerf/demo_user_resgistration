2f0ae1fa6f12bc4c55ed5c2bf47e90f45a60b05d
mikeyb <MichaelBrown172@Gmail.com>
2019-05-14 21:00:09 -0400

    v1.0.2 --> v1.0.3
            1. Implementing Angular front end..... :  FIXED
            2. Front end doesn't do much at the moment.
    	3. Working...

--- bin/main/static/js/app.js
+++ bin/main/static/js/app.js
@@ -1,29 +1,47 @@
-var app = angular.module('dur',
-    ['ngRoute','ngResource']
-);
+//var app = angular.module('dur',['ngRoute','ngResource']);
 
 
-app.config(function($routerProvider){
+var app = angular.module('dur',['ngRoute']);
+app.config(function($routeProvider){
 
     $routeProvider.when(
         '/list-all-users',{
-            templateUrl:'/template/listuser.html',
+            templateUrl:'template/listuser.html',
             controller:'listUserController'
             }
         ).when(
              '/register-new-user',{
-                 templateUrl:'/template/userregistration.html',
+                 templateUrl:'template/userregistration.html',
                  controller:'registerUserController'
                  }
         ).when(
                '/update-user/:id',{
-                templateUrl:'/template/userupdation.html',
+                templateUrl:'template/userupdation.html',
                 controller:'userDetailsController'
                 }
         ).otherwise({
                 redirectTo:'/home',
-                templateUrl:'/template/home.html',
+                templateUrl:'template/home.html',
                 }
         );
     }
 );
+
+
+
+app.controller("listUserController", function ($scope) {
+           
+    $scope.message = "listUserController";
+
+});
+
+app.controller("registerUserController", function ($scope) {
+    $scope.message = "registerUserController";
+});
+
+
+
+app.controller("userDetailsController", function ($scope) {
+    $scope.message = "userDetailsController";
+});
+
