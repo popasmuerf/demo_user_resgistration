8840aef90bfb6d52ec393fbc5ff2a1d6449a62a6
mikeyb <MichaelBrown172@Gmail.com>
2019-05-25 19:30:39 -0400

    v1.0.3 --> v1.0.4
    UI is working.

--- bin/main/static/js/app.js
+++ bin/main/static/js/app.js
@@ -35,8 +35,23 @@ app.controller("listUserController", function ($scope) {
 
 });
 
-app.controller("registerUserController", function ($scope) {
-    $scope.message = "registerUserController";
+app.controller("registerUserController", function ($scope,$http,$location,$route) {
+    $scope.submitUserForm = function(){
+        $http({
+            method:"POST",
+            url:"http://localhost:8080/api/user/",
+            data:$scope.user,
+        })
+        .then(function(response){
+            $location.path("/list-all-users");
+            $route.reload() ;
+        },function(errResponse){
+            $scope.errorMessage = errResponse.data.errorMessage;
+        });
+    }
+    $scope.resetForm = function(){
+        $scope.user = null ;
+    };
 });
 
 
